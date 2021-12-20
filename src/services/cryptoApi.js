81ba9d810589d20d979e0baeb7c21c015849ecaa
Benedict Kpaduwa <42049724+Benedict-Kpaduwa@users.noreply.github.com>
2021-12-20 22:20:28 +0100

    Add the service for the exchange rates

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,30 +1,32 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
-    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    'x-rapidapi-key': '1935c71faamsh00746550dcb5120p1289bcjsn7d6f8134391f'
+  'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+  'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
 };
 
-const baseUrl = "https://coinranking1.p.rapidapi.com";
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
-const createRequest = (url) => ({ url, headers: cryptoApiHeaders})
-
-export const cryptoApi  = createApi({
-    reducerPath: "cryptoApi",
-    baseQuery: fetchBaseQuery({ baseUrl }),
-    endpoints: ( builder ) => ({
-        getCryptos: builder.query({
-            query: (count) => createRequest(`/coins?limit=${count}`),
-        }),
-        getCryptoDetails: builder.query({
-            query: (coinId) => createRequest(`/coin/${coinId}`)
-        })
-    })
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_CRYPTO_API_URL }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+    getExchanges: builder.query({
+      query: () => createRequest('/exchanges'),
+    }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history/${timeperiod}`),
+    }),
+  }),
 });
 
-export const {
-    useGetCryptosQuery, useGetCryptoDetailsQuery,
-} = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetExchangesQuery, useGetCryptoHistoryQuery } = cryptoApi;
 
 
 
